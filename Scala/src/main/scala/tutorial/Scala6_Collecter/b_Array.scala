ff4b9139d169b686eb16e0f53d02bc448fd461e3
李橐驼 <478551557@qq.com>
2021-03-30 11:47:53 +0800

    all MyScalaTutorial file

--- Scala/src/main/scala/tutorial/Scala6_Collecter/b_Array.scala
+++ Scala/src/main/scala/tutorial/Scala6_Collecter/b_Array.scala
@@ -10,7 +10,7 @@ object b_Array {
 
   //todo 不可变数组
   //1)	基本语法
-  def mainImmutable1(args: Array[String]): Unit = {
+  def immutable1(args: Array[String]): Unit = {
     //（1）数组定义
     val arr = new Array[Int](4)
     println(arr.length) // 4
@@ -39,18 +39,16 @@ object b_Array {
   }
 
   //2)	基本操作
-  def mainImmutable2(args: Array[String]): Unit = {
+  def immutable2(args: Array[String]): Unit = {
     // 创建数组的另外一种方式
     val arr1 = Array(1,2,3,4)
     val arr2 = Array(5,6,7,8)
     // 添加数组元素，创建新数组
     val arr3: Array[Int] = arr1 :+ 5
     println( arr1 == arr3 ) // false
-
     val arr4: Array[Int] = arr1 ++: arr2
     // 添加集合
     val arr5: Array[Int] = arr1 ++ arr2
-
     arr4.foreach(println)
     println("****************")
     arr5.foreach(println)
@@ -61,11 +59,9 @@ object b_Array {
     // 合并数组
     val arr6: Array[Int] = Array.concat(arr1, arr2)
     arr6.foreach(println)
-
     // 创建指定范围的数组
     val arr7: Array[Int] = Array.range(0,2)
     arr7.foreach(println)
-
     // 创建并填充指定数量的数组
     val arr8:Array[Int] = Array.fill[Int](5)(-1)
     arr8.foreach(println)
@@ -74,7 +70,7 @@ object b_Array {
   //todo 可变数组
   //1)	基本语法
   import scala.collection.mutable.ArrayBuffer
-  def mainMutable1(args: Array[String]): Unit = {
+  def mutable1(args: Array[String]): Unit = {
     val buffer = new ArrayBuffer[Int]
     // 增加数据
     buffer.append(1,2,3,4)
@@ -93,7 +89,7 @@ object b_Array {
   }
   //2)	基本操作
   //    import scala.collection.mutable.ArrayBuffer
-  def mainMutable2(args: Array[String]): Unit = {
+  def mutable2(args: Array[String]): Unit = {
     val buffer1 = ArrayBuffer(1,2,3,4)
     val buffer2 = ArrayBuffer(5,6,7,8)
 
@@ -110,11 +106,10 @@ object b_Array {
 
   //todo 可变数组和不可变数组转换
   import scala.collection.mutable
-  //    import scala.collection.mutable.ArrayBuffer
-  def mainSwitch(args: Array[String]): Unit = {
+//  import scala.collection.mutable.ArrayBuffer
+  def switch(args: Array[String]): Unit = {
     val buffer = ArrayBuffer(1,2,3,4)
     val array = Array(4,5,6,7)
-
     // 将不可变数组转换为可变数组
     val buffer1: mutable.Buffer[Int] = array.toBuffer
     // 将可变数组转换为不可变数组
--- Scala/src/main/scala/tutorial/Scala6_Collecter/c_Seq.scala
+++ Scala/src/main/scala/tutorial/Scala6_Collecter/c_Seq.scala
@@ -11,10 +11,8 @@ object c_Seq {
   //todo 不可变List
   //1)	基本语法
   def main1(args: Array[String]): Unit = {
-
     // Seq集合
     val list = List(1,2,3,4)
-
     // 增加数据
     val list1: List[Int] = list :+ 1
     println(list1 eq list)
@@ -28,22 +26,18 @@ object c_Seq {
   }
   //2)	基本操作
   def main2(args: Array[String]): Unit = {
-
     // Seq集合
     val list1 = List(1,2,3,4)
     // 空集合
     val list2: List[Nothing] = List()
-    val nil  = Nil
+    val nil = Nil
     println(list2 eq nil)
-
     // 创建集合
-    val list3: List[Int]  = 1::2::3::Nil
-    val list4: List[Int] = list1 ::: Nil
-
+    val list3: List[Int] = 1::2::3::Nil
+    val list4: List[Int] = list1 ::: Nil//扁平化操作
     // 连接集合
     val list5: List[Int] = List.concat(list3, list4)
     list5.foreach(println)
-
     // 创建一个指定重复数量的元素列表
     val list6: List[String] = List.fill[String](3)("a")
     list6.foreach(println)
@@ -70,20 +64,16 @@ object c_Seq {
   //2)	基本操作
   import scala.collection.mutable.ListBuffer
   def main4(args: Array[String]): Unit = {
-
     // 可变集合
     val buffer1 = ListBuffer(1,2,3,4)
     val buffer2 = ListBuffer(5,6,7,8)
-
     // 增加数据
     val buffer3: ListBuffer[Int] = buffer1 :+ 5
     val buffer4: ListBuffer[Int] = buffer1 += 5
     val buffer5: ListBuffer[Int] = buffer1 ++ buffer2
     val buffer6: ListBuffer[Int] = buffer1 ++= buffer2
-
     println( buffer5 eq buffer1 )
     println( buffer6 eq buffer1 )
-
 //    val buffer7: ListBuffer[Int] = buffer1 - 2
     val buffer8: ListBuffer[Int] = buffer1 -= 2
 //    println( buffer7 eq buffer1 )
@@ -94,15 +84,11 @@ object c_Seq {
   import scala.collection.mutable
   import scala.collection.mutable.ListBuffer
   def main5(args: Array[String]): Unit = {
-
     val buffer = ListBuffer(1,2,3,4)
     val list = List(5,6,7,8)
-
     // 可变集合转变为不可变集合
     val list1: List[Int] = buffer.toList
     // 不可变集合转变为可变集合
     val buffer1: mutable.Buffer[Int] = list.toBuffer
   }
-
-
 }
